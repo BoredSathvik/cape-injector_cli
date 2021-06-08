cef4c962185661306cce4b62c2bc3608f530cce7
srmdev <68980580+srmdev@users.noreply.github.com>
2021-06-08 11:09:32 +0530

    Added error checks + made it so iobitunlocker does not need to be in path

--- index.ts
+++ index.ts
@@ -7,24 +7,32 @@ const mcNames: { x64: string; x32: string } = await (
 const mcName = parseInt(Deno.env.get('architecture_bits')!) == 32 ? mcNames.x32 : mcNames.x64;
 
 const programfiles = Deno.env.get('programfiles') ?? 'C:/Program Files';
+const programfiles_86 = Deno.env.get('ProgramFiles(x86)') ?? 'C:/Program Files (x86)';
+
+const iobitPath = join(programfiles_86, 'IObit/IObit Unlocker');
+
 const mcPath = mcName;
 const skinPack = join(programfiles, 'WindowsApps', mcPath, 'data/skin_packs');
 const persona = join(skinPack, 'persona');
 
-if (!Deno.args[0]) Deno.exit(0x1);
+if (!exists(iobitPath))
+	throw new Error(
+		'IoBit unlocker not found pls install it before running this (https://www.iobit.com/en/iobit-unlocker.php)'
+	);
+if (!Deno.args[0]) throw new Error('Arguments not found');
 
 switch (Deno.args[0].toLowerCase()) {
 	case '/clear':
 		{
 			const a = Deno.run({
-				cmd: ['iobitunlocker', '/Delete', persona],
+				cmd: [join(iobitPath, 'iobitunlocker.exe'), '/Delete', persona],
 			});
 			await a.status();
 
 			if (!(await exists('persona'))) await Deno.mkdir('persona');
 
 			const b = Deno.run({
-				cmd: ['iobitunlocker', '/Copy', join(Deno.cwd(), 'persona'), skinPack],
+				cmd: [join(iobitPath, 'iobitunlocker.exe'), '/Copy', join(Deno.cwd(), 'persona'), skinPack],
 			});
 			await b.status();
 		}
@@ -32,15 +40,15 @@ switch (Deno.args[0].toLowerCase()) {
 
 	case '/inject':
 		{
-			if (!Deno.args[1]) Deno.exit(0x3);
-			if (!(await exists(Deno.args[1]))) Deno.exit(0x3);
+			if (!Deno.args[1]) throw new Error('Path to cape pack not found');
+			if (!(await exists(Deno.args[1]))) throw new Error('Invalid path');
 			const paths: string[] = [];
 			for await (const cape of Deno.readDir(Deno.args[1])) {
 				paths.push(`"${join(Deno.args[1], cape.name)}"`);
 			}
 
 			const c = Deno.run({
-				cmd: ['iobitunlocker', '/Copy', `"${paths.join(',')}"`, persona],
+				cmd: [join(iobitPath, 'iobitunlocker.exe'), '/Copy', `"${paths.join(',')}"`, persona],
 			});
 
 			await c.status();
@@ -48,15 +56,11 @@ switch (Deno.args[0].toLowerCase()) {
 		break;
 
 	case '/help':
-		console.log(
-			'capeinjector_1.16.22101.0_x64 --clear\ncapeinjector_1.16.22101.0_x64 --inject "pathToCapePack"'
-		);
+		console.log('cpi_1.16.221 --clear\ncpi_1.16.221 --inject "pathToCapePack"');
 		break;
 
 	default:
-		console.log(
-			'capeinjector_1.16.22101.0_x64 --clear\ncapeinjector_1.16.22101.0_x64 --inject "pathToCapePack"'
-		);
+		console.log('cpi_1.16.221 --clear\ncpi_1.16.221 --inject "pathToCapePack"');
 
 		break;
 }
